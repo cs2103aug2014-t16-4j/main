2bdf1201647997e3bd358c97d7c7d06f594e0c3e
Kaung Htet Aung <tommy.aung7@gmail.com>
2014-09-17 08:34:59 +0800

    Added JUnit test file

---
+++ test/gui/TextBuddyUITest.java
@@ -0,0 +1,50 @@
+package gui;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TextBuddyUITest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSearch() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSort() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testClear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDisplay() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
