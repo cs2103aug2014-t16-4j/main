4cb9fe3d84212c44b9aba7e34711688438866245
giong_000 <giong_000@172.19.206.184>
2014-09-26 18:30:38 +0800

    Add more fields to Task

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,9 +1,42 @@
 package model;
 
+import java.util.Date;
+
 public class Task {
 	private String name;
 	private String description;
+	private Date date;
+	private int priority;
+	private int frequency;
 
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public int getPriority() {
+		return priority;
+	}
+	
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+	public int getFrequency() {
+		return frequency;
+	}
+	
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	
+	public Task() {
+		
+	}
+	
 	public Task(String name) {
 		this.name = name;
 	}
