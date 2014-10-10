00e190bf5a41fbaee8e797c71353cc254a64aa2d
Kaung Htet Aung <tommy.aung7@gmail.com>
2014-10-11 02:55:40 +0800

    Added delete function

--- src/logic/Display.java
+++ src/logic/Display.java
@@ -1,14 +1,5 @@
 package logic;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class Display extends Command{
 	String fileName;
--- src/logic/LogicController.java
+++ src/logic/LogicController.java
@@ -1,28 +1,78 @@
 package logic;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import model.Task;
 
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class LogicController {
+	public static ArrayList <JSONObject> tasksBuffer;
 	String fileName;
-	static ArrayList <JSONObject> tasksBuffer;
-	CommandEnum selectedCommand = CommandEnum.INVALID;
-	Add add;
+	Add logicAdd;
+	Clear logicClear;
+	Delete logicDelete;
+	
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 
 	public LogicController(String fileName) {
 		this.fileName = fileName;
 		tasksBuffer = new ArrayList<JSONObject>();
 	}
 	
+	//Fetch all tasks from file in the beginning.	
+	public void init() throws IOException
+	{
+		JSONParser jsonParser = new JSONParser();
+		String line;
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(fileName));
+			while ((line = in.readLine()) != null) {
+				JSONObject obj = (JSONObject) jsonParser.parse(line);
+				tasksBuffer.add(obj);
+			}
+			in.close();
+		} catch (FileNotFoundException | ParseException e) {
+			e.printStackTrace();
+		}
+	}
+		
 	public boolean add(Task task){
-		add = new Add();
-		add.setFileName(fileName);
-		return add.executeCommand();
+		logicAdd = new Add();
+		logicAdd.setFileName(fileName);
+		logicAdd.setTask(task);
+		return logicAdd.executeCommand();
 	}
 	
+	public boolean clear(){
+		logicClear = new Clear();
+		logicClear.setFileName(fileName);
+		return logicClear.executeCommand();
+	}
+	
+	public String delete(JSONObject task){
+		logicDelete = new Delete();
+		logicDelete.setFileName(fileName);
+		logicDelete.setTask(task);
+		if(logicDelete.executeCommand()){
+			return String.format(Consts.STRING_DELETE, fileName,task.get(Consts.NAME));
+		}else{
+			return Consts.USAGE_DELETE;
+		}
+	}
+
 	public Task jsonToTask(JSONObject obj){
 		Task temp = null;
 		try{
@@ -33,7 +83,6 @@ public class LogicController {
 			temp.setFrequency((int) obj.get(Consts.FREQUENCY));
 			temp.setPriority((int) obj.get(Consts.PRIORITY));
 		}catch(Exception e){
-			//e.printStackTrace();
 		}
 		return temp;	
 	}
