8522dd59b4ad73559809dd9f627c3463adfd2693
Kaung Htet Aung <tommy.aung7@gmail.com>
2014-10-15 15:28:50 +0800

    Added raw google class
    just the authentication part

--- src/logic/Consts.java
+++ src/logic/Consts.java
@@ -28,6 +28,8 @@ public final class Consts {
     public static final String STRING_DELETE = "deleted from %s: \"%s\"";
     public static final String STRING_UPDATE = "%s is updated.\n";
 	public static final String STRING_NOT_UPDATE = "%s is not updated.\n";
+	public static final String STRING_CRE_CORRECT = "The credentials are correct.";
+	public static final String STRING_CRE_NOT_CORRECT = "The credentials are not correct.";
     public static final DateFormat formatter = new SimpleDateFormat("dd/M/yyyy HH:mm:ss");
 	public static final SimpleDateFormat cmpFormatter = new SimpleDateFormat("yyyyMMdd");
     
---
+++ src/logic/GoogleCal.java
@@ -0,0 +1,28 @@
+package logic;
+
+import com.google.gdata.client.calendar.CalendarService;
+import com.google.gdata.util.AuthenticationException;
+
+public class GoogleCal {
+	private static final String URL = "https://www.google.com/calendar/feeds/default/private/full";
+	private String appName = "";
+	String username,password;
+	CalendarService client;
+
+	public GoogleCal(String appName){
+		this.appName = appName;
+	}
+	
+	public String authenticate(String username,String password){
+		this.username = username;
+		this.password = password;
+		this.client = new CalendarService(appName);
+		try {
+			this.client.setUserCredentials(username, password);
+			return Consts.STRING_CRE_CORRECT;
+		} catch (AuthenticationException e) {
+			return Consts.STRING_CRE_NOT_CORRECT;
+		}
+	}
+	
+}
