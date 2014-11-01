fd1b35b601b825fb0030ff929a8bb17c1883f83b
Kaung Htet Aung <tommy.aung7@gmail.com>
2014-11-01 12:21:53 +0800

    Added one way sync
    Need to fix some bugs such as start time and end time

--- src/logic/GoogleCal.java
+++ src/logic/GoogleCal.java
@@ -36,23 +36,25 @@ public class GoogleCal {
 	HttpTransport httpTransport;
 	JacksonFactory jsonFactory;
 	private static Calendar client;
-	
-	public static boolean isOnline(){
+
+	public static boolean isOnline() {
 		Socket sock = new Socket();
-		InetSocketAddress addr = new InetSocketAddress("www.google.com",80);
-		try{
+		InetSocketAddress addr = new InetSocketAddress("www.google.com", 80);
+		try {
 			sock.connect(addr);
 			return true;
-		}catch(IOException e){
+		} catch (IOException e) {
 			System.err.println("User is offline.");
 			e.printStackTrace();
 			return false;
-		}finally{
+		} finally {
 			try {
 				sock.close();
-			}catch(IOException e){}
+			} catch (IOException e) {
+			}
 		}
 	}
+
 	public void setUp() {
 
 	}
@@ -63,110 +65,94 @@ public class GoogleCal {
 		String appName = "TaskBox";
 		String clientId = "743259209106-g4qtcmneg0dhi9efos04d46bnnjiiich.apps.googleusercontent.com";
 		String clientSecret = "AyJjPfMtT0gQPki-eArk4xKG";
-		//String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
+		// String redirectUrl = "urn:ietf:wg:oauth:2.0:oob";
 
-		flow = new GoogleAuthorizationCodeFlow.Builder(
-				httpTransport, jsonFactory, clientId, clientSecret,
+		flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport,
+				jsonFactory, clientId, clientSecret,
 				Arrays.asList(CalendarScopes.CALENDAR)).build();
 
 		String url = flow.newAuthorizationUrl().setRedirectUri(redirectUrl)
 				.build();
-		
+
 		return url;
 		/*
-		System.out.println("Please open the following URL in your browser then type the authorization code:");
-		System.out.println("  " + url);
-		System.out.println("What is the authorization code?");
-		String code = new Scanner(System.in).nextLine();
-
-		AuthorizationCodeTokenRequest tokenRequest = flow.newTokenRequest(code)
-				.setRedirectUri(redirectUrl);
-		TokenResponse tokenRes = null;
-		try {
-			tokenRes = tokenRequest.execute();
-		} catch (IOException e) {
-			System.err.println("Token request failed");
-		}
-		if (tokenRes != null) {
-			writeFile(tokenRes.getAccessToken());
-			return tokenRes.getAccessToken();
-		} else {
-			return "Couldn't get token";
-		}
-		*/
+		 * System.out.println(
+		 * "Please open the following URL in your browser then type the authorization code:"
+		 * ); System.out.println("  " + url);
+		 * System.out.println("What is the authorization code?"); String code =
+		 * new Scanner(System.in).nextLine();
+		 * 
+		 * AuthorizationCodeTokenRequest tokenRequest =
+		 * flow.newTokenRequest(code) .setRedirectUri(redirectUrl);
+		 * TokenResponse tokenRes = null; try { tokenRes =
+		 * tokenRequest.execute(); } catch (IOException e) {
+		 * System.err.println("Token request failed"); } if (tokenRes != null) {
+		 * writeFile(tokenRes.getAccessToken()); return
+		 * tokenRes.getAccessToken(); } else { return "Couldn't get token"; }
+		 */
 	}
-	
-	public boolean withExistingToken(){
+
+	public boolean withExistingToken() {
 		TokenResponse tokenRes = new TokenResponse();
-		if(validFile()){
+		if (validFile()) {
 			tokenRes.setAccessToken(readFile());
-			try{
-				Credential credential = flow.createAndStoreCredential(tokenRes, appName);
+			try {
+				Credential credential = flow.createAndStoreCredential(tokenRes,
+						appName);
 				HttpRequestInitializer initializer = credential;
-				Calendar.Builder builder = new Calendar.Builder(httpTransport, jsonFactory,initializer);
+				Calendar.Builder builder = new Calendar.Builder(httpTransport,
+						jsonFactory, initializer);
 				builder.setApplicationName(appName);
 				client = builder.build();
 				return true;
-			}catch(Exception e){
+			} catch (Exception e) {
 				return false;
 			}
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
-	public boolean generateNewToken(String code) throws IOException{
-		TokenResponse tokenRes = new TokenResponse();
-		try{
-			if(validFile()){
-				tokenRes.setAccessToken(readFile());
-			}
-		}catch(Exception e){
-			AuthorizationCodeTokenRequest tokenRequest = flow.newTokenRequest(code).setRedirectUri(redirectUrl);
+
+	public boolean generateNewToken(String code) throws IOException {
+		try {
+			TokenResponse tokenRes = new TokenResponse();
+			AuthorizationCodeTokenRequest tokenRequest = flow.newTokenRequest(code)
+					.setRedirectUri(redirectUrl);
 			tokenRes = tokenRequest.execute();
 			writeFile(tokenRes.getAccessToken());
-			Credential credential = flow.createAndStoreCredential(tokenRes, appName);
+			Credential credential = flow
+					.createAndStoreCredential(tokenRes, appName);
 			HttpRequestInitializer initializer = credential;
-			Calendar.Builder builder = new Calendar.Builder(httpTransport, jsonFactory,initializer);
+			Calendar.Builder builder = new Calendar.Builder(httpTransport,
+					jsonFactory, initializer);
 			builder.setApplicationName(appName);
 			client = builder.build();
 			return true;
-		}
-		return false;
-	}
-	
-	public String syncGCal() throws IOException{
-		TokenResponse tokenRes = new TokenResponse();
-		//try {
-		//	if(validFile()){
-		//		tokenRes.setAccessToken(readFile());
-		//	}else{
-				//AuthorizationCodeTokenRequest tokenRequest = flow.newTokenRequest(code).setRedirectUri(redirectUrl);
-				//tokenRes = tokenRequest.execute();
-	//		}
-	//	} catch (IOException e) {
-	//		e.printStackTrace();
-	//		System.err.println("Token request failed");
-	//	}
-		if (tokenRes != null) {
-			com.google.api.services.calendar.model.Calendar calendar = client.calendars().get("primary").execute();
-			System.out.println(calendar.getSummary());
-			//for(JSONObject i:LogicController.tasksBuffer){
-			//	createEvent(Converter.jsonToTask(i));
-			//}
-			return Consts.STRING_SYNC_COMPLETE;
-		} else {
-			return Consts.STRING_SYNC_NOT_COMPLETE;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
 	}
 
-	public String createEvent(Task tsk) throws IOException{
-		Event event= new Event();
+	public String syncGCal() throws IOException {
+			com.google.api.services.calendar.model.Calendar calendar = client
+					.calendars().get("primary").execute();
+			//System.out.println(calendar.getId());
+			for(JSONObject i:LogicController.tasksBuffer){
+				createEvent(Converter.jsonToTask(i),calendar.getId());
+			}
+			return Consts.STRING_SYNC_COMPLETE;
+	}
+
+	public String createEvent(Task tsk,String calId) throws IOException {
+		Event event = new Event();
 		event.setSummary(tsk.getName());
 		event.setDescription(tsk.getDescription());
-		event.setStart(new EventDateTime().setDateTime(new DateTime(tsk.getStartDate())));
-		event.setEnd(new EventDateTime().setDateTime(new DateTime(tsk.getEndDate())));
-		client.events().insert("primary", event).execute();
+		event.setStart(new EventDateTime().setDateTime(new DateTime(tsk
+				.getStartDate())));
+		event.setEnd(new EventDateTime().setDateTime(new DateTime(tsk
+				.getEndDate())));
+		client.events().insert(calId, event).execute();
 		return event.getId();
 	}
 
@@ -182,8 +168,8 @@ public class GoogleCal {
 			return false;
 		}
 	}
-	
-	public static void clearFile(){
+
+	public static void clearFile() {
 		try {
 			FileWriter fstream = new FileWriter("GoogleToken", false);
 			BufferedWriter bufferedWriter = new BufferedWriter(fstream);
@@ -193,11 +179,12 @@ public class GoogleCal {
 			e.printStackTrace();
 		}
 	}
-	
-	public static String readFile(){
+
+	public static String readFile() {
 		String token = "";
 		try {
-			BufferedReader in = new BufferedReader(new FileReader("GoogleToken"));
+			BufferedReader in = new BufferedReader(
+					new FileReader("GoogleToken"));
 			token = in.readLine();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -205,16 +192,16 @@ public class GoogleCal {
 		}
 		return token;
 	}
-	
-	public static boolean validFile(){
+
+	public static boolean validFile() {
 		File f = new File("GoogleToken");
-		if(f.exists()){
-			if(readFile().equals("")){
+		if (f.exists()) {
+			if (readFile().equals("")) {
 				return false;
-			}else{
+			} else {
 				return true;
 			}
-		}else{
+		} else {
 			return false;
 		}
 	}
