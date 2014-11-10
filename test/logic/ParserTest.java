dc49d5ef695dc9f0aea4204aee9b094b58994b7b
Thanh <giongto35@yahoo.com>
2014-11-10 21:55:25 +0800

    +Parser Test

--- test/logic/ParserTest.java
+++ test/logic/ParserTest.java
@@ -14,13 +14,13 @@ public class ParserTest {
 	@Test
 	public void test() {
 		assertEquals("{\"Name\":\"a task\",\"Status\":1,\"Description\":\"some desc\",\"EndDate\":\"11\\/11\\/2014 23:59:59\",\"StartDate\":\"11\\/11\\/2014 00:00:00\",\"Frequency\":0,\"Priority\":1}",
-				Converter.taskToJSON(parser.decompose("a task day after today (some desc) important")));
-		assertEquals("",
-				Converter.taskToJSON(parser.decompose("an impotant task 12/15/2014 to 12/19/2014 (some desc)")));
-		assertEquals("", 
-				Converter.taskToJSON(parser.decompose("a task today daily")));
-		assertEquals("",
-				Converter.taskToJSON(parser.decompose("a task on the day after day after tomorrow weekly")));
+				Converter.taskToJSON(parser.decompose("a task day after today (some desc) important")).toString());
+		assertEquals("{\"Name\":\"an impotant task\",\"Status\":1,\"Description\":\"some desc\",\"EndDate\":\"19\\/12\\/2014 23:59:59\",\"StartDate\":\"15\\/12\\/2014 00:00:00\",\"Frequency\":0,\"Priority\":0}",
+				Converter.taskToJSON(parser.decompose("an impotant task 12/15/2014 to 12/19/2014 (some desc)")).toString());
+		assertEquals("{\"Name\":\"a task\",\"Status\":1,\"Description\":\"\",\"EndDate\":\"10\\/11\\/2014 23:59:59\",\"StartDate\":\"10\\/11\\/2014 00:00:00\",\"Frequency\":1,\"Priority\":0}", 
+				Converter.taskToJSON(parser.decompose("a task today daily")).toString());
+		assertEquals("{\"Name\":\"a task\",\"Status\":1,\"Description\":\"\",\"EndDate\":\"13\\/11\\/2014 23:59:59\",\"StartDate\":\"13\\/11\\/2014 00:00:00\",\"Frequency\":2,\"Priority\":0}",
+				Converter.taskToJSON(parser.decompose("a task on the day after day after tomorrow weekly")).toString());
 	}
 
 }
